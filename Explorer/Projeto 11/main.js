484a6a262b46b2e1f2c6cf4857d3a52dbd7c0924
Arthur Dias Gomes Gerhardt <66851119+arthurgerhardt@users.noreply.github.com>
2024-08-26 14:57:48 -0300

    Add files via upload

---
+++ Explorer/Projeto 11/main.js
@@ -0,0 +1,44 @@
+// Variáveis
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+
+// Gerando número aleatório
+let randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+// Eventos
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener("keydown", function (event) {
+    if (event.key == "Enter" && screen1.classList.contains("hide")) {
+        handleResetClick(event);
+    }
+})
+
+// Função callback
+function handleTryClick(event) {
+  event.preventDefault();
+  const inputNumber = document.querySelector("#inputNumber");
+
+  if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen();
+    screen2.querySelector('h2').innerText = `Acertou em ${xAttempts} tentativas`;
+  }
+
+  inputNumber.value = '';
+  xAttempts++;
+}
+
+function handleResetClick() {
+  toggleScreen();
+  xAttempts = 1;
+  randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
+
+
